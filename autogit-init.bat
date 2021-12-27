7aa387e5064eee3518b7c469a4810d2efc3061c9
dcheva <dmitry.cheva@gmail.com>
2021-12-27 08:40:38 +0200

    Update github init script

--- autogit-init.bat
+++ autogit-init.bat
@@ -1,8 +1,13 @@
+cd %1
 del .git
 git init
-echo #%1 > README.MD
 git add -A
-git commit -m "first commit"
+git commit -m Initial
+git checkout -b main
 git branch -M main
 git remote add origin https://github.com/dcheva/%1.git
-git push -u origin main
+git push --set-upstream origin main
+echo # %1 GitHub repo > README.MD
+git add -A
+git commit -m Add README.MD
+git push --set-upstream origin main
