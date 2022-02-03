f472d2c7ac0be741992015aef3a2dfac2355b845
dcheva <dmytro.cheva@gmail.com>
2022-02-03 22:11:31 +0200

    up

--- autogit-init.bat
+++ autogit-init.bat
@@ -3,11 +3,11 @@ del .git
 git init
 git add -A
 git commit -m Initial
-git checkout -b main
-git branch -M main
+git checkout -b initial
+git branch -M initial
 git remote add origin https://github.com/dcheva/%1.git
-git push --set-upstream origin main
+git push --set-upstream origin initial
 echo # %1 GitHub repo > README.MD
 git add -A
 git commit -m Add README.MD
-git push --set-upstream origin main
+git push --set-upstream origin initial
