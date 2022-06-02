d9fcdd7997a831442db3f820ec0a898532d745ac
Dmytro Cheva <dmytro.cheva@gmail.com>
2022-06-02 17:04:17 +0300

    up

--- poweroff.bat
+++ poweroff.bat
@@ -1,1 +1,1 @@
-pushd C:\Projects && autogit-all && shutdown /h /f /t 10 -y
+pushd C:\Projects && autogit-all && shutdown /h
--- reboot.bat
+++ reboot.bat
@@ -1,1 +1,1 @@
-pushd C:\Projects && autogit-all && shutdown /r /f /t 10 -y
+pushd C:\Projects && autogit-all && shutdown /r
