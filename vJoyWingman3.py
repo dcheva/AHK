c3ba9ccc00c0893d496369b859503926c64a274a
Dmytro Cheva <dmytro.cheva@gmail.com>
2022-10-04 01:36:08 +0300

    vJoyWingman3.py working StepN switcher

--- vJoyWingman3.py
+++ vJoyWingman3.py
@@ -5,66 +5,61 @@ import time
 if starting:
 	vJoy_Key = Key.CapsLock # кнопка на клавиатуре включающая режим управления джойстика мышкой
 	steps = [0,4,8,12,16,20,24,28,32,-32,-28,-24,-20,-16,-12,-8,-4]
-	currentX = 0
-	lastX = 0
+	oldJoyX = 0
+	newJoyX = 0
+	oldX = 0
 	newX = 0
-	currentStep = 0
-	currentStepN = -0
-	lastStep = 0
+	oldStepN = 0
+	newStepN = 0
+	oldStep = 0
+	newStep = 0
+	m = ''
 	
 ################################################################
 ## Let's dance
 ################################################################
 
-currentX = joystick[1].x
-cp = currentX - lastX
-if cp != 0: 
-	currentStep = cp
+newJoyX = joystick[1].x
 
-if lastX <> currentX:
-	if currentStep > 0 and lastStep < 0:
-		currentStepN = 0
-	if currentStep < 0 and lastStep > 0:
-		currentStepN = 0
-	if currentStep > 0 and lastStep > 0:
-		currentStepN += 1
-	if currentStep < 0 and lastStep < 0:
-		currentStepN -= 1
-	if currentStepN > 8:
-		currentStepN = 8
-	if currentStepN < -8:
-		currentStepN = -8
-		
-	if abs(lastX + steps[currentStepN]) < abs(currentX):
-		if currentStepN == 0 and currentX > lastX:
-			currentStepN = 1
-		elif currentStepN == 0 and currentX < lastX:
-			currentStepN = -1
-		newX = lastX + steps[currentStepN]
-		currentStep = steps[currentStepN]
-	else: 
-		newX = currentX
+if oldJoyX == newJoyX: #->
+	newStepN = oldStepN
+	newX = oldX
+	m = '-> ' + m
 	
-lastX = currentX
-lastStep = currentStep
+else:
+	if oldJoyX < newJoyX: #->r
+		if oldStepN <= 0: #cl->r 
+			newStepN = 1
+			m = 'cl->r ' + m
+		if oldStepN > 0: #r->r 
+			newStepN += 1
+			m = 'r->r ' + m
+	if oldJoyX > newJoyX: #->l
+		if oldStepN >= 0: #cr->l 
+			newStepN = -1
+			m = 'cr->l ' + m
+		if oldStepN < 0: #l->l 
+			newStepN -= 1
+			m = 'l->l ' + m
+
+oldStepN = newStepN
+oldJoyX = newJoyX
+oldX = newX
 
 vJoy[0].x = newX
 vJoy[0].y = joystick[1].y
 vJoy[0].z = joystick[1].z
 
 ################################################################
-diagnostics.watch(currentStepN)
-diagnostics.watch(lastStep)
-diagnostics.watch(lastX)
-diagnostics.watch(currentStep)
-diagnostics.watch(currentX)
+diagnostics.watch(m)
 diagnostics.watch(newX)
+diagnostics.watch(oldX)
+diagnostics.watch(newStepN)
+diagnostics.watch(oldStepN)
 ################################################################
 diagnostics.watch(joystick[1].x)
-diagnostics.watch(joystick[1].y)
-diagnostics.watch(joystick[1].z)
 ################################################################
 diagnostics.watch(vJoy[0].x)
-diagnostics.watch(vJoy[0].y)
-diagnostics.watch(vJoy[0].z)
 ################################################################
+
+time.sleep(1)
