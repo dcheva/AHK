0185bc213bb35dd2ff1e5fda51a7c794b77508e0
Dmytro Cheva <dmytro.cheva@gmail.com>
2022-10-04 14:51:59 +0300

    Correct map keys in vJoyWingman3.py

--- vJoyWingman3.py
+++ vJoyWingman3.py
@@ -1,6 +1,6 @@
 import time
-#from System import Int16
-#from ctypes import windll, Structure, c_ulong, byref
+from System import Int16
+from ctypes import windll, Structure, c_ulong, byref
    
 if starting:
 	vJoy_Key = Key.CapsLock # кнопка на клавиатуре включающая режим управления джойстика мышкой
@@ -14,8 +14,45 @@ if starting:
 	oldStep = 0
 	newStep = 0
 	m = ''
-	
-	vJoy[0].setButton(0,mouse.getButton(0)) 
+
+################################################################
+## Map 32 keys
+################################################################
+
+vJoy[0].setButton(0,mouse.getButton(0)) 
+
+vJoy[0].setButton(0,int(keyboard.getKeyDown(Key.NumberPad1)))
+vJoy[0].setButton(1,int(keyboard.getKeyDown(Key.NumberPad2)))
+vJoy[0].setButton(2,int(keyboard.getKeyDown(Key.NumberPad3)))
+vJoy[0].setButton(3,int(keyboard.getKeyDown(Key.NumberPad4)))
+vJoy[0].setButton(4,int(keyboard.getKeyDown(Key.NumberPad5)))
+vJoy[0].setButton(5,int(keyboard.getKeyDown(Key.NumberPad6)))
+vJoy[0].setButton(6,int(keyboard.getKeyDown(Key.NumberPad7)))
+vJoy[0].setButton(7,int(keyboard.getKeyDown(Key.NumberPad8)))
+vJoy[0].setButton(8,int(keyboard.getKeyDown(Key.NumberPad9)))
+vJoy[0].setButton(9,int(keyboard.getKeyDown(Key.NumberPad0)))
+vJoy[0].setButton(10,int(keyboard.getKeyDown(Key.NumberPadComma)))
+vJoy[0].setButton(11,int(keyboard.getKeyDown(Key.NumberPadEnter)))
+vJoy[0].setButton(12,int(keyboard.getKeyDown(Key.NumberPadPlus)))
+vJoy[0].setButton(13,int(keyboard.getKeyDown(Key.NumberPadMinus)))
+vJoy[0].setButton(14,int(keyboard.getKeyDown(Key.NumberPadEquals)))
+vJoy[0].setButton(15,int(keyboard.getKeyDown(Key.NumberLock)))
+vJoy[0].setButton(16,int(keyboard.getKeyDown(Key.NumberPadSlash)))
+vJoy[0].setButton(17,int(keyboard.getKeyDown(Key.NumberPadStar)))
+vJoy[0].setButton(18,int(keyboard.getKeyDown(Key.NumberPadPeriod)))
+vJoy[0].setButton(19,int(keyboard.getKeyDown(Key.RightControl))) 
+vJoy[0].setButton(20,int(keyboard.getKeyDown(Key.RightShift)))
+vJoy[0].setButton(21,int(keyboard.getKeyDown(Key.LeftArrow)))
+vJoy[0].setButton(22,int(keyboard.getKeyDown(Key.UpArrow)))
+vJoy[0].setButton(23,int(keyboard.getKeyDown(Key.DownArrow)))
+vJoy[0].setButton(24,int(keyboard.getKeyDown(Key.RightArrow)))
+vJoy[0].setButton(25,int(keyboard.getKeyDown(Key.D1)))
+vJoy[0].setButton(26,int(keyboard.getKeyDown(Key.D2)))
+vJoy[0].setButton(27,int(keyboard.getKeyDown(Key.D3)))
+vJoy[0].setButton(28,int(keyboard.getKeyDown(Key.D4)))
+vJoy[0].setButton(29,int(keyboard.getKeyDown(Key.PrintScreen)))
+vJoy[0].setButton(30,int(keyboard.getKeyDown(Key.ScrollLock)))
+vJoy[0].setButton(31,int(keyboard.getKeyDown(Key.Pause)))
 
 ################################################################
 ## Let's dance
