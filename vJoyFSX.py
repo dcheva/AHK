626244bdb1b31c618b728baf69849761a0d5214b
Dmytro Cheva <dmytro.cheva@gmail.com>
2023-04-15 18:59:08 +0300

    New

--- vJoyFSX.py
+++ vJoyFSX.py
@@ -2,6 +2,7 @@ from System import Int16
 from ctypes import windll, Structure, c_ulong, byref
 import winsound, time
 
+penta = [131,165,196,220,262,330,392,440,524,660,784,880,1047,1319,1568,1760,2093]
 
 class POINT(Structure):
    _fields_ = [("x", c_ulong), ("y", c_ulong)]
@@ -11,16 +12,20 @@ if starting:
 	vJoy_Enabled = False # данный флаг используется для временного отключения джойстика мышкой
 	vJoy_Key = Key.CapsLock # кнопка на клавиатуре включающая режим управления джойстика мышкой
 	vJoy_Reset = Key.Backspace # Центровка по Backspace
-	vJoy_LControl = Key.LeftControl # Проверка по Control
-	vJoy_RControl = Key.RightControl # Проверка по Control
-	vJoy_Alt = Key.LeftAlt # Альтернативное управление - ось Х на руль
+	vJoy_LControl = Key.LeftControl # Привязка к Control
+	vJoy_RControl = Key.RightControl # Привязка к Control
+	vJoy_Control = False # флаг нажатой клавиши Control
+	vJoy_LAlt = Key.LeftAlt # Привязка к Alt
+	vJoy_RAlt = Key.RightAlt # Привязка к Alt
+	vJoy_Alt = False # Альтернативное управление - ось Х на руль
+	vJoy_Control = False # флаг нажатой клавиши Control
 	vJoy_Freeview = False # флаг временного выключения режима управления джойстика мышкой
 	Freeview = False # флаг временного выключения режима управления джойстика мышкой
-	screen_x = windll.user32.GetSystemMetrics(0) / 2 # размер экрана
-	screen_y = windll.user32.GetSystemMetrics(1) / 2
+	screen_x = int(windll.user32.GetSystemMetrics(0) / 2) # размер экрана
+	screen_y = int(windll.user32.GetSystemMetrics(1) / 2)
 	sx = screen_x
 	sy = screen_y
-	slider = -32768 / 2
+	slider = int(-32768 / 2)
 	pt = POINT()
 	
 	# Внимание - не стоит задавать близкие значения Scale_V и Scale_R, так как это приведет к взаимовлиянию настроек. Одно из значений в паре пусть будет 100.
@@ -41,40 +46,55 @@ if starting:
 def stat(Joy_stat, count=2):
 	for i in range(count):
 		if Joy_stat:
-			winsound.Beep(300,30)
+			winsound.Beep(penta[5],50)
 		else:
-			winsound.Beep(200,50)
+			winsound.Beep(penta[3],50)
 		
 ## Let's dance
 Freeview = mouse.rightButton # MSFS:: правая кнопка на мышке включает свободный обзор. 
 ## mouse.middleButton .rightButton .leftButton - средняя, правая или левая кнопка мыши и т.п.
 ## Freeview = keyboard.getKeyDown(Key.V) # пример для кнопки на клавиатуре для свободного обзора
-vJoy[0].setButton(0,mouse.getButton(0))
 
-if keyboard.getPressed(vJoy_LControl) or keyboard.getPressed(vJoy_RControl):
-	stat(Joy_stat)
+## ЛКМ на кнопку джоя - кнопку тормоза
+if Joy_stat: vJoy[0].setButton(0, mouse.getButton(0))
+
+## Привязка статуса Control
+if keyboard.getKeyDown(vJoy_LControl) or keyboard.getKeyDown(vJoy_RControl): vJoy_Control = True
+else: vJoy_Control = False
+
+## Привязка статуса Alt
+if keyboard.getKeyDown(vJoy_LAlt) or keyboard.getKeyDown(vJoy_RAlt): vJoy_Alt = True
+else: vJoy_Alt = False
 
 # Включение и отключение джойстика
 if keyboard.getPressed(vJoy_Key):
 	if Joy_stat:
 		Joy_stat = False
 		vJoy_Enabled = False
-		vJoy[0].setButton(0,False)		
+		## Обязательно - отключение привязки ЛКМ к кнопке тормоза
+		vJoy[0].setButton(0, False)
 		# vJoy[0].x = 0 # при отключении джойстик встает в 0
    		# vJoy[0].y = 0
    		# vJoy[0].z = 0
    		# vJoy[0].rz = 0
-   		winsound.Beep(300,30)
-   		winsound.Beep(200,50)
+   		## Озвучка переключения - откл
+   		winsound.Beep(penta[5],50)
+   		winsound.Beep(penta[3],50)
 	else:
 		vJoy_Enabled = True
-		vJoy[0].setButton(0,mouse.getButton(0))
-   		winsound.Beep(200,50)
-   		winsound.Beep(300,30)
+		vJoy[0].setButton(0, mouse.getButton(0))
+   		## Озвучка переключения - вкл
+   		winsound.Beep(penta[3],50)
+   		winsound.Beep(penta[5],50)
 		
 if keyboard.getPressed(vJoy_Reset):
-		vJoy[0].x = 0  # Центровка по Backspace - джойстик встает в 0
-   		vJoy[0].y = 0
+	if Joy_stat:
+		## Центровка по Backspace - элероны в 0
+		vJoy[0].x = 0  
+   		## Выравнивание руля высоты - только с Контролом
+   		if vJoy_Control:
+			vJoy[0].y = 0
+   		## Выравнивание руля направления
    		vJoy[0].z = 0
    		vJoy[0].rz = 0
 		# move mouse to vJoy position
@@ -82,19 +102,15 @@ if keyboard.getPressed(vJoy_Reset):
 		sy = vJoy[0].y * screen_y / (32768 / 2) * axisy_inversion + screen_y;
 		windll.user32.SetCursorPos(sx, sy) # автоцентрирование курсора мыши
 		stat(Joy_stat)
-   		winsound.Beep(400,30)
+   		winsound.Beep(penta[7],50)
 
 if vJoy_Enabled:
 	if Freeview: # деактивация/активация джойстика, если нажата кнопка мыши
 		Joy_stat = False
-		vJoy[0].setButton(0,False) 
-		# vJoy[0].x = 0 # при отключении джойстик встает в 0
-   		# #vJoy[0].y = 0
-   		# vJoy[0].z = 0
-   		# vJoy[0].rz = 0
+		vJoy[0].setButton(0, False)
 	if not Freeview and Joy_stat == False:
 		Joy_stat = True
-		vJoy[0].setButton(0,mouse.getButton(0)) 
+		vJoy[0].setButton(0 ,mouse.getButton(0)) 
 		# move mouse to vJoy position
 		sx = vJoy[0].x * screen_x / (32768 / 2) * axisx_inversion + screen_x;
 		sy = vJoy[0].y * screen_y / (32768 / 2) * axisy_inversion + screen_y;
@@ -110,25 +126,19 @@ if Joy_stat:
 	# далее идет умножение на масштаб оси к экрану и задание инверсии, если она есть
 	x = (mouse_x - screen_x) * multipler_x / preci * scale_Vx / scale_Rx * axisx_inversion
 	y = (mouse_y - screen_y) * multipler_y / preci * scale_Vy / scale_Ry * axisy_inversion
-	if keyboard.getKeyDown(vJoy_Alt): # Альтернативное управление - ось Х на руль 
+	if vJoy_Alt: # Альтернативное управление - ось Х на руль 
 		vJoy[0].z = x * scale_Vz / scale_Rz * axisz_inversion
 	else:
 		vJoy[0].x = x
 	vJoy[0].y = y
 	vJoy[0].rz = vJoy[0].z
 	
-	# scrol to throttle
-	if mouse.wheelUp:
-		slider += 32768 / 20; # 5%
-		winsound.Beep(int(slider/100+400),50)
-		
-	if mouse.wheelDown:
-		slider -= 32768 / 20; # 5%
-		winsound.Beep(int(slider/100+400),50)
-		
+	## scrol to throttle
+	if mouse.wheelUp: slider += 32768 / 20; # 5%
+	if mouse.wheelDown: slider -= 32768 / 20; # 5%
 	if slider < -32768 / 2: slider = -32768 / 2;	
-	if slider > 32768 / 2: slider = 32768 / 2;	
-
+	if slider > 32768 / 2: slider = 32768 / 2;
+	if vJoy[0].slider != slider: winsound.Beep(int((slider+32768)/penta[0]),50)
 	vJoy[0].slider = slider;
 
 diagnostics.watch(Freeview)
---
+++ vJoyWingman4.py
@@ -0,0 +1,127 @@
+from System import Int16
+from ctypes import windll, Structure, c_ulong, byref
+import winsound, time
+
+
+if starting:
+	steps = [0,4,8,12,16,20,24,28,32,-32,-28,-24,-20,-16,-12,-8,-4]
+	oldJoyX = 0
+	newJoyX = 0
+	oldX = 0
+	newX = 0
+	oldStepN = 0
+	newStepN = 0
+	oldStep = 0
+	newStep = 0
+	m = ''
+
+
+################################################################
+## Map 32 keys
+################################################################
+
+#vJoy[0].setButton(0,mouse.getButton(0)) 
+#vJoy[0].setButton(1,mouse.getButton(1)) 
+#vJoy[0].setButton(2,mouse.getButton(2)) 
+vJoy[0].setButton(0,int(keyboard.getKeyDown(Key.NumberPad1)))
+vJoy[0].setButton(1,int(keyboard.getKeyDown(Key.NumberPad2)))
+vJoy[0].setButton(2,int(keyboard.getKeyDown(Key.NumberPad3)))
+vJoy[0].setButton(3,int(keyboard.getKeyDown(Key.NumberPad4)))
+vJoy[0].setButton(4,int(keyboard.getKeyDown(Key.NumberPad5)))
+vJoy[0].setButton(5,int(keyboard.getKeyDown(Key.NumberPad6)))
+vJoy[0].setButton(6,int(keyboard.getKeyDown(Key.NumberPad7)))
+vJoy[0].setButton(7,int(keyboard.getKeyDown(Key.NumberPad8)))
+vJoy[0].setButton(8,int(keyboard.getKeyDown(Key.NumberPad9)))
+vJoy[0].setButton(9,int(keyboard.getKeyDown(Key.NumberPad0)))
+vJoy[0].setButton(10,int(keyboard.getKeyDown(Key.NumberPadComma)))
+vJoy[0].setButton(11,int(keyboard.getKeyDown(Key.NumberPadEnter)))
+vJoy[0].setButton(12,int(keyboard.getKeyDown(Key.NumberPadPlus)))
+vJoy[0].setButton(13,int(keyboard.getKeyDown(Key.NumberPadMinus)))
+vJoy[0].setButton(14,int(keyboard.getKeyDown(Key.NumberPadEquals)))
+vJoy[0].setButton(15,int(keyboard.getKeyDown(Key.NumberLock)))
+vJoy[0].setButton(16,int(keyboard.getKeyDown(Key.NumberPadSlash)))
+vJoy[0].setButton(17,int(keyboard.getKeyDown(Key.NumberPadStar)))
+vJoy[0].setButton(18,int(keyboard.getKeyDown(Key.NumberPadPeriod)))
+vJoy[0].setButton(19,int(keyboard.getKeyDown(Key.RightControl))) 
+vJoy[0].setButton(20,int(keyboard.getKeyDown(Key.RightShift)))
+vJoy[0].setButton(21,int(keyboard.getKeyDown(Key.LeftArrow)))
+vJoy[0].setButton(22,int(keyboard.getKeyDown(Key.UpArrow)))
+vJoy[0].setButton(23,int(keyboard.getKeyDown(Key.DownArrow)))
+vJoy[0].setButton(24,int(keyboard.getKeyDown(Key.RightArrow)))
+vJoy[0].setButton(25,int(keyboard.getKeyDown(Key.D1)))
+vJoy[0].setButton(26,int(keyboard.getKeyDown(Key.D2)))
+vJoy[0].setButton(27,int(keyboard.getKeyDown(Key.D3)))
+vJoy[0].setButton(28,int(keyboard.getKeyDown(Key.D4)))
+vJoy[0].setButton(29,int(keyboard.getKeyDown(Key.PrintScreen)))
+vJoy[0].setButton(30,int(keyboard.getKeyDown(Key.ScrollLock)))
+vJoy[0].setButton(31,int(keyboard.getKeyDown(Key.Pause)))
+
+
+################################################################
+## Let's dance
+################################################################
+
+newJoyX = joystick[1].x
+
+if oldJoyX == newJoyX: #->
+	newStepN = oldStepN
+	newX = oldX
+	m = '-> '
+	
+else:
+	if oldJoyX < newJoyX: #->r
+		if oldStepN <= 0: #cl->r 
+			newStepN = 1
+			m = 'cl->r '
+		if oldStepN > 0: #r->r 
+			newStepN += 1
+			m = 'r->r '
+	if oldJoyX > newJoyX: #->l
+		if oldStepN >= 0: #cr->l 
+			newStepN = -1
+			m = 'cr->l '
+		if oldStepN < 0: #l->l 
+			newStepN -= 1
+			m = 'l->l '
+
+if newStepN > 8: newStepN = 8
+if newStepN <-8: newStepN =-8
+newStep = steps[newStepN]
+
+if oldJoyX <> newJoyX: #!->
+	dJoyX = newJoyX - oldJoyX
+	if abs(dJoyX) > 64: newStep = dJoyX 
+	newX = oldX + newStep
+	m = '!-> ' + str(dJoyX)
+
+#if newStepN > 0 and (newX > newJoyX or newStep > dJoyX): newX = newJoyX
+#if newStepN < 0 and (newX < newJoyX or newStep > dJoyX): newX = newJoyX
+if newStepN > 0 and newX > newJoyX: newX = newJoyX
+if newStepN < 0 and newX < newJoyX: newX = newJoyX
+
+oldStep = newStep
+oldStepN = newStepN
+oldJoyX = newJoyX
+oldX = newX
+
+vJoy[0].x = newX
+vJoy[0].y = joystick[1].y
+vJoy[0].z = joystick[1].z
+
+################################################################
+diagnostics.watch(m)
+diagnostics.watch(newX)
+diagnostics.watch(oldX)
+diagnostics.watch(newStepN)
+diagnostics.watch(oldStepN)
+diagnostics.watch(newStep)
+diagnostics.watch(oldStep)
+################################################################
+diagnostics.watch(joystick[1].x)
+################################################################
+diagnostics.watch(vJoy[0].x)
+diagnostics.watch(vJoy[0].y)
+diagnostics.watch(vJoy[0].z)
+################################################################
+
+#time.sleep(0.5)
