4d98a829dff47f9998979a04ae887353caf0d0a1
Dmytro Cheva <dmytro.cheva@gmail.com>
2023-04-16 23:44:32 +0300

    Update sysrestore.cmd

--- sysrestore.cmd
+++ sysrestore.cmd
@@ -1,1 +1,6 @@
-start SFC /SCANNOW
+@echo:
+@echo Download and Run "services repair tool" by ESET ServicesRepair.exe https://www.wintips.org/wp-content/uploads/files/ServicesRepair.zip
+@echo Download and Run Sysinspector by ESET sysinspector_nt64_enu.exe https://www.eset.com/int/support/sysinspector/#download
+@echo:
+@echo Now starting scan...
+SFC /SCANNOW
