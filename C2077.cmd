6d7d11546556981043922687dfba5ecc0bfaa143
Dmytro Cheva <dmytro.cheva@gmail.com>
2023-10-03 19:45:21 +0300

    Offline update all

---
+++ C2077.cmd
@@ -0,0 +1,3 @@
+del /F /Q "C:\Documents and Settings\Dmytro\AppData\Local\NVIDIA\DXCache\*.bin" "C:\Documents and Settings\Dmytro\AppData\Local\NVIDIA\DXCache\*.toc"
+start C:\Projects\C2077\autogit-this.bat Autogit-this %date% at %time:~0,2%:%time:~3,2%:%time:~6,2%
+start Cyberpunk2077.exe
