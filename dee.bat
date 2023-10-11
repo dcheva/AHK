211f3fabd57f1aa9ce755c43955c8463f686290d
Dmytro Cheva <dmytro.cheva@gmail.com>
2023-10-12 01:27:40 +0300

    Add Art and Dee batches for Cyberpank

--- dee.bat
+++ dee.bat
@@ -1,7 +1,7 @@
 del /F /Q "C:\Documents and Settings\Dmytro\AppData\Local\NVIDIA\DXCache\*.bin" "C:\Documents and Settings\Dmytro\AppData\Local\NVIDIA\DXCache\*.toc"
 pushd C:\Projects\C2077
 git add -A
-git commit -am "Autogit-art %date% at %time:~0,2%:%time:~3,2%:%time:~6,2%"
-git checkout art
+git commit -am "Autogit-dee %date% at %time:~0,2%:%time:~3,2%:%time:~6,2%"
+git checkout dee
 pushd C:\Games\Cyberpunk.2077.2.0\bin\x64
 start Cyberpunk2077.exe
