e427b52ac12e510e2f3d306f735d6088f34149d7
Dmytro Cheva <dmytro.cheva@gmail.com>
2023-10-12 01:31:51 +0300

    up

--- C2077.cmd
+++ C2077.cmd
@@ -2,6 +2,8 @@ del /F /Q "C:\Documents and Settings\Dmytro\AppData\Local\NVIDIA\DXCache\*.bin" 
 pushd C:\Projects\C2077
 git add -A
 git commit -am "Autogit-art %date% at %time:~0,2%:%time:~3,2%:%time:~6,2%"
+git pull
+git push
 git checkout art
 pushd C:\Games\Cyberpunk.2077.2.0\bin\x64
 start Cyberpunk2077.exe
