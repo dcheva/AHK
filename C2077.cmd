4da14ac58d55c78843884a853ba8c8c14ef0d28c
Dmytro Cheva <dmytro.cheva@gmail.com>
2023-10-12 20:10:47 +0300

    up

--- C2077.cmd
+++ C2077.cmd
@@ -1,7 +1,7 @@
 del /F /Q "C:\Documents and Settings\Dmytro\AppData\Local\NVIDIA\DXCache\*.bin" "C:\Documents and Settings\Dmytro\AppData\Local\NVIDIA\DXCache\*.toc"
 pushd C:\Projects\C2077
 git add -A
-git commit -am "Autogit-art %date% at %time:~0,2%:%time:~3,2%:%time:~6,2%"
+git commit -am "C:\Projects\C2077\art.bat %date% at %time:~0,2%:%time:~3,2%:%time:~6,2%"
 git pull
 git push
 git checkout art
