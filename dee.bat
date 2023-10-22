59bd9b17e5b4f1a0dba3968229b6e743e6ec9c97
Dmytro Cheva <dmytro.cheva@gmail.com>
2023-10-22 04:49:28 +0300

    Update all

--- dee.bat
+++ dee.bat
@@ -1,4 +1,4 @@
-del /F /Q "C:\Documents and Settings\Dmytro\AppData\Local\NVIDIA\DXCache\*.bin" "C:\Documents and Settings\Dmytro\AppData\Local\NVIDIA\DXCache\*.toc"
+;del /F /Q "C:\Documents and Settings\Dmytro\AppData\Local\NVIDIA\DXCache\*.bin" "C:\Documents and Settings\Dmytro\AppData\Local\NVIDIA\DXCache\*.toc"
 pushd C:\Projects\C2077
 git add -A
 git commit -am "Autogit-dee %date% at %time:~0,2%:%time:~3,2%:%time:~6,2%"
