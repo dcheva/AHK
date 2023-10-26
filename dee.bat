9dff5af8735dbed85491106fdd19d3fad7b2d403
Dmytro Cheva <dmytro.cheva@gmail.com>
2023-10-26 17:10:43 +0300

    Update all

--- dee.bat
+++ dee.bat
@@ -1,10 +1,9 @@
 ;del /F /Q "C:\Documents and Settings\Dmytro\AppData\Local\NVIDIA\DXCache\*.bin" "C:\Documents and Settings\Dmytro\AppData\Local\NVIDIA\DXCache\*.toc"
 pushd C:\Projects\C2077
 git add -A
-git commit -am "Autogit-dee %date% at %time:~0,2%:%time:~3,2%:%time:~6,2%"
+git commit -am "C:\Projects\C2077\dee.bat %date% at %time:~0,2%:%time:~3,2%:%time:~6,2%"
 git pull
 git push
 git checkout dee
 pushd C:\Games\Cyberpunk.2077.2.0\bin\x64
-cd C:\Games\Cyberpunk.2077.2.0\bin\x64
-start C:\Games\Cyberpunk.2077.2.0\bin\x64\Cyberpunk2077.exe
+start Cyberpunk2077.exe
