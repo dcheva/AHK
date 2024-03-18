e55c9eeb0439eee748b1918fde369721cdd2b3f6
Dmytro Cheva <dmytro.cheva@gmail.com>
2024-03-18 14:35:32 +0200

    Change github.com to bitbucket.org

--- autogit-init.bat
+++ autogit-init.bat
@@ -6,7 +6,8 @@ rem git remote add origin https://github.com/dcheva/%project%.git
 git remote add origin https://dcheusov@bitbucket.org/dcheusov24/%project%.git
 git pull origin %branch%
 
-echo # %project% GitHub repo >> README.MD
+rem echo # %project% GitHub repo >> README.MD
+echo # %project% ~GitHub~ BitBucket repo >> README.MD
 git add -A
 git commit -m "Add README.MD"
 git branch --set-upstream-to=origin/%branch% %branch%
