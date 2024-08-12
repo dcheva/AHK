44771c902bfe05ea51fe64456445d0631e2c6101
Dmytro Cheva <dmytro.cheva@gmail.com>
2024-08-12 15:19:07 +0300

    up

--- autogit-init.bat
+++ autogit-init.bat
@@ -2,12 +2,15 @@ for %%I in (.) do set project=%%~nxI
 set branch=initial
 
 git init -b %branch%
+
 git remote add origin https://github.com/dcheva/%project%.git
 git remote add bucket https://dcheva@bitbucket.org/dcheva/%project%.git
-git pull origin %branch%
 
-rem echo # %project% GitHub repo >> README.MD
-echo # %project% BitBucket repo >> README.MD
+rem git pull origin %branch%
+rem git pull bucket %branch%
+
+echo # %project% Git and Bucket repo >> README.MD
+
 git add -A
 git commit -m "Add README.MD"
 git branch --set-upstream-to=origin/%branch% %branch%
