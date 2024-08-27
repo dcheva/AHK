c3e1622d2241501c0fe733451e1615da55f2d8b1
Dmytro Cheva <dmytro.cheva@gmail.com>
2024-08-27 04:20:28 +0300

    autogit.bat         autogit.bat0 autogit.bat1 autogit.bat2 autogit.bat3 autogit.bat4 autogit.bat5 autogit.bat6 autogit.bat7 autogit.bat8 autogit.bat9 0

--- autogit.bat
+++ autogit.bat
@@ -1,5 +1,5 @@
 git add -A
-git commit -am "%1 %2 %3 %4 %5 %6 %7 %8 %9"
+git commit -am "%1 %2 %3 %4 %5 %6 %7 %8 %9 %10 %11 %12 %13 %14 %15 %16 %17 %18 %19 %20"
 git fetch
 git merge -X theirs
 git commit -am "merged theirs"
