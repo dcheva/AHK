d595008e93dc14cebe7252e68e21a5e29fe52ce2
Dmytro Cheva <dmytro.cheva@gmail.com>
2024-09-05 10:30:32 +0300

    buckett

--- autogit-off-line.bat
+++ autogit-off-line.bat
@@ -1,5 +1,6 @@
 git add -A
 git commit -m "%1 %2 %3 %4 %5 %6 %7 %8 %9"
+@echo off
 @rem ---- command interpretator ----
 IF /I "%1" == "loop" (
     @echo on
--- autogit-pull.bat
+++ autogit-pull.bat
@@ -2,6 +2,7 @@ git add -A
 git commit -m "%1 %2 %3 %4 %5 %6 %7 %8 %9"
 git pull
 git push origin HEAD
+@echo off
 @rem ---- command interpretator ----
 IF /I "%1" == "loop" (
     @echo on
