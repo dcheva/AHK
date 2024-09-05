91914dc14d530a85fff7e4a42c529391fc244b37
Dmytro Cheva <dmytro.cheva@gmail.com>
2024-09-05 08:57:34 +0300

    merged theirs

--- autogit.bat
+++ autogit.bat
@@ -1,8 +1,8 @@
 git add -A
-git commit -am "%1 %2 %3 %4 %5 %6 %7 %8 %9"
+git commit -a -m "%1 %2 %3 %4 %5 %6 %7 %8 %9"
 git fetch
 git merge -X theirs
-git commit -am "merged theirs"
+git commit -a -m "merged theirs"
 @echo off
 @rem ---- command interpretator ----
 IF /I "%1" == "bucket" (
@@ -15,7 +15,7 @@ IF /I "%1" == "loop" (
     :loop
     timeout /t "%2"
     git add -A
-    git commit -am "%date% at %time:~0,2%:%time:~3,2%:%time:~6,2%"
+    git commit -a -m "%date% at %time:~0,2%:%time:~3,2%:%time:~6,2%"
     git pull
     git push origin HEAD
     goto loop
