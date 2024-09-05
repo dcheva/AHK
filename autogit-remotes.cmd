7101c6b4b1d3fbb95a87d75b5efddfadf241b82c
Dmytro Cheva <dmytro.cheva@gmail.com>
2024-09-05 08:53:03 +0300

    merged theirs

--- autogit-remotes.cmd
+++ autogit-remotes.cmd
@@ -5,7 +5,7 @@ cd "%%d"
 @echo off
 @echo
 @echo // in "%%d" \\ --------------------------------
-autogit "Update remotes %date% at %time:~0,2%:%time:~3,2%:%time:~6,2%"
+autogit Update remotes "%date% at %time:~0,2%:%time:~3,2%:%time:~6,2%"
 autogit bucket
 cd ..
 )
