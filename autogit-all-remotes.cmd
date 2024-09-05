013f89f700cb570f66d44f5f07ea5f51b33452ac
Dmytro Cheva <dmytro.cheva@gmail.com>
2024-09-05 09:48:10 +0300

    "Update-remotes-05-Sep-24-09:39:58.34        "

--- autogit-all-remotes.cmd
+++ autogit-all-remotes.cmd
@@ -3,7 +3,7 @@ cd .
 for /d %%d in (*.*) DO (
 cd "%%d"
 @echo off
-@echo  //        \\
+@echo  //
 @echo // in "%%d" \\ --------------------------------
 @echo "\"autogit Update-remotes-%date%-%TIME: =0%\""
 autogit "\"autogit Update-remotes-%date%-%TIME: =0%\""
--- autogit.bat
+++ autogit.bat
@@ -3,7 +3,7 @@ git commit -a
 git commit -m "\"%1 %2 %3 %4 %5 %6 %7 %8 %9\""
 git fetch
 git merge -X theirs
-git commit -am "merged theirs"
+git commit -m "merged theirs"
 @echo off
 @rem ---- command interpretator ----
 IF /I "%1" == "bucket" (
@@ -16,7 +16,8 @@ IF /I "%1" == "loop" (
     :loop
     timeout /t "%2"
     git add -A
-    git commit -am "\"Autogit-loop-%date%-%TIME: =0%\""
+    git add -a
+    git commit -m "\"Autogit-loop-%date%-%TIME: =0%\""
     git pull
     git push origin HEAD
     goto loop
