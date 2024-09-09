8c96469d4af3d46331b6ef3350cf5bc0096f61f6
cheva <dmitry.cheva@gmail.com>
2024-09-09 09:36:40 +0300

    Change autogit-pull Add zip2all

--- autogit-pull.bat
+++ autogit-pull.bat
@@ -1,16 +1,1 @@
-git add -A
-git commit -m "%1 %2 %3 %4 %5 %6 %7 %8 %9"
 git pull
-git push origin HEAD
-@echo off
-@rem ---- command interpretator ----
-IF /I "%1" == "loop" (
-    @echo on
-    :loop
-    timeout /t "%2"
-    git add -A
-    git commit -m "Autogit-pull-loop-%date%-%TIME: =0%"
-    git pull
-    git push origin HEAD
-    goto loop
-)
---
+++ autogit-zip2ll.bat
@@ -0,0 +1,2 @@
+@echo on
+for /D %%d in (*.*) do "C:\Program Files\7-Zip\7z.exe" a -tzip -mx2 "%%d.zip" ".\%%d\*"
