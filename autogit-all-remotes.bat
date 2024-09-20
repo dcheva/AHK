ce5ea000e13f1ce419b13b4e2042fb7008aebb4d
Dee <dee.covan@gmail.com>
2024-09-20 10:13:58 +0300

    Update-remotes-20-Sep-24-10:13:58.14

--- autogit-all-remotes.bat
+++ autogit-all-remotes.bat
@@ -5,8 +5,16 @@ cd "%%d"
 cd %%d
 pushd .
 start autogit "Update-remotes-%date%-%TIME: =0%"
+cd ..
+pushd .
+)
 timeout /t 5 /nobreak >nul
-start autogit bucket
+for /d %%d in (*.*) DO (
+cd "%%d"
+@echo // in "%%d" \\ --------------------------------
+cd %%d
+pushd .
+start autogit bucket "Update-remotes-%date%-%TIME: =0%"
 cd ..
 pushd .
 )
